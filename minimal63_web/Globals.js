28cee6f12c15df5ac9adf8e6da8d655b1d4009a5
Mike <dm5xx@gmx.de>
2020-11-25 17:22:52 +0100

    removed minimal control and custom from project

--- minimal63_web/Globals.js
+++
@@ -1,7 +0,0 @@
-for(var a = 0; a < numberOfBoards; a++)
-{
-    window["statusB"+a] = 0;
-}
-
-var lockSwitchStatus = false;
-
--- minimal63_web/GroupDef_c.js
+++
@@ -1,9 +0,0 @@
-var groupB0 = {
-    group0 : [ 0,1,2],
-    group1 : [ 3,4,5],
-}
-
-var groupB1 = {
-    group0 : [ 7,8,9],
-    group1 : [ 3,4,5],
-}
